03b513e5462b1ddcb71c52785812536a891d857f
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-05-31 20:18:39 -0400

    Capstone Lab 4 - Task List Revisited

---
+++ Capstone4-TaskListRevisted/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Capstone4_TaskListRevisted.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Capstone4-TaskListRevisted/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone4_TaskListRevisted.Models
+{
+    public partial class Tasks
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool? Completed { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
